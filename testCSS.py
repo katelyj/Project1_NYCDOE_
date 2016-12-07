fb903c585de323103b1a34ce84b4c05130e413f8
katelyj <kjohnston@stuy.edu>
2016-12-06 20:39:27 -0500

    navbar

---
+++ testCSS.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, url_for, session, redirect
+import hashlib, sqlite3
+#from utils import 
+
+app = Flask(__name__)
+app.secret_key = '<j\x9ch\x80+\x0b\xd2\xb6\n\xf7\x9dj\xb8\x0fmrO\xce\xcd\x19\xd49\xe5S\x1f^\x8d\xb8"\x89Z'
+
+@app.route("/")
+def test():
+	return render_template("streamingPage.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
